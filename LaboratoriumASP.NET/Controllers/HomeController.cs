3404d2a82de4b985d73d7814c925e792ca4bb09f
YauheniWsei <yauheni.papavets@microsoft.wsei.edu.pl>
2024-10-09 11:46:25 +0200

    Zadanie 2

--- LaboratoriumASP.NET/Controllers/HomeController.cs
+++ LaboratoriumASP.NET/Controllers/HomeController.cs
@@ -33,8 +33,38 @@ public class HomeController : Controller
      * Zadanie 1
      */
 
-    public IActionResult Calculator()
+    public IActionResult Calculator(int q)
     {
+        var op = Request.Query["op"];
+        var x = double.Parse(Request.Query["x"]);
+        var y = double.Parse(Request.Query["y"]);
+
+        var result = 0.0d;
+
+        switch (op)
+        {
+            case "add":
+                result = x + y;
+                ViewBag.Operator = " + ";
+                break;
+            case "sub":
+                result = x - y;
+                ViewBag.Operator = " - ";
+                break;
+            case "mul":
+                result = x * y;
+                ViewBag.Operator = " * ";
+                break;
+            case "div":
+                result = x / y;
+                ViewBag.Operator = " / ";
+                break;
+        }
+
+        ViewBag.Result = result;
+        ViewBag.X = x;
+        ViewBag.Y = y;
+
         return View();
     }
 
